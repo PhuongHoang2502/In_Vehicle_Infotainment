2153bb2b1bfcaf0ef6a0b25e1a6d5c05fb4c7eeb
KistLeo <dinh.nguyenbku@gmail.com>
2025-04-13 04:11:39 -0700

    test update feature

---
+++ build-Tesla-Desktop-Debug/moc_qmlinterface.cpp
@@ -0,0 +1,393 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'qmlinterface.h'
+**
+** Created by: The Qt Meta Object Compiler version 67 (Qt 5.12.8)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include "../Tesla/Music/cpp/qmlinterface.h"
+#include <QtCore/qbytearray.h>
+#include <QtCore/qmetatype.h>
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'qmlinterface.h' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 67
+#error "This file was generated using the moc from 5.12.8. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+QT_BEGIN_MOC_NAMESPACE
+QT_WARNING_PUSH
+QT_WARNING_DISABLE_DEPRECATED
+struct qt_meta_stringdata_QmlInterface_t {
+    QByteArrayData data[34];
+    char stringdata0[503];
+};
+#define QT_MOC_LITERAL(idx, ofs, len) \
+    Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
+    qptrdiff(offsetof(qt_meta_stringdata_QmlInterface_t, stringdata0) + ofs \
+        - idx * sizeof(QByteArrayData)) \
+    )
+static const qt_meta_stringdata_QmlInterface_t qt_meta_stringdata_QmlInterface = {
+    {
+QT_MOC_LITERAL(0, 0, 12), // "QmlInterface"
+QT_MOC_LITERAL(1, 13, 24), // "currentMusicIndexChanged"
+QT_MOC_LITERAL(2, 38, 0), // ""
+QT_MOC_LITERAL(3, 39, 17), // "musicIndexChanged"
+QT_MOC_LITERAL(4, 57, 14), // "shuffleChanged"
+QT_MOC_LITERAL(5, 72, 13), // "repeatChanged"
+QT_MOC_LITERAL(6, 86, 20), // "musicListSizeChanged"
+QT_MOC_LITERAL(7, 107, 4), // "data"
+QT_MOC_LITERAL(8, 112, 21), // "playerPositionChanged"
+QT_MOC_LITERAL(9, 134, 20), // "musicDurationChanged"
+QT_MOC_LITERAL(10, 155, 26), // "onCurrentMusicIndexChanged"
+QT_MOC_LITERAL(11, 182, 30), // "onCurrentPlayerPositionChanged"
+QT_MOC_LITERAL(12, 213, 27), // "onCurrentMediaStatusChanged"
+QT_MOC_LITERAL(13, 241, 25), // "QMediaPlayer::MediaStatus"
+QT_MOC_LITERAL(14, 267, 6), // "status"
+QT_MOC_LITERAL(15, 274, 16), // "getAllAudioFiles"
+QT_MOC_LITERAL(16, 291, 12), // "getMusicPath"
+QT_MOC_LITERAL(17, 304, 4), // "QDir"
+QT_MOC_LITERAL(18, 309, 12), // "setMusicPath"
+QT_MOC_LITERAL(19, 322, 4), // "path"
+QT_MOC_LITERAL(20, 327, 12), // "startPlaying"
+QT_MOC_LITERAL(21, 340, 12), // "pausePlaying"
+QT_MOC_LITERAL(22, 353, 11), // "stopPlaying"
+QT_MOC_LITERAL(23, 365, 8), // "playNext"
+QT_MOC_LITERAL(24, 374, 12), // "playPrevious"
+QT_MOC_LITERAL(25, 387, 18), // "removeFromPlaylist"
+QT_MOC_LITERAL(26, 406, 5), // "index"
+QT_MOC_LITERAL(27, 412, 10), // "setShuffle"
+QT_MOC_LITERAL(28, 423, 5), // "state"
+QT_MOC_LITERAL(29, 429, 15), // "setPlaybackMode"
+QT_MOC_LITERAL(30, 445, 4), // "mode"
+QT_MOC_LITERAL(31, 450, 15), // "setPlayingState"
+QT_MOC_LITERAL(32, 466, 15), // "setCurrentIndex"
+QT_MOC_LITERAL(33, 482, 20) // "getCurrentMusicIndex"
+
+    },
+    "QmlInterface\0currentMusicIndexChanged\0"
+    "\0musicIndexChanged\0shuffleChanged\0"
+    "repeatChanged\0musicListSizeChanged\0"
+    "data\0playerPositionChanged\0"
+    "musicDurationChanged\0onCurrentMusicIndexChanged\0"
+    "onCurrentPlayerPositionChanged\0"
+    "onCurrentMediaStatusChanged\0"
+    "QMediaPlayer::MediaStatus\0status\0"
+    "getAllAudioFiles\0getMusicPath\0QDir\0"
+    "setMusicPath\0path\0startPlaying\0"
+    "pausePlaying\0stopPlaying\0playNext\0"
+    "playPrevious\0removeFromPlaylist\0index\0"
+    "setShuffle\0state\0setPlaybackMode\0mode\0"
+    "setPlayingState\0setCurrentIndex\0"
+    "getCurrentMusicIndex"
+};
+#undef QT_MOC_LITERAL
+
+static const uint qt_meta_data_QmlInterface[] = {
+
+ // content:
+       8,       // revision
+       0,       // classname
+       0,    0, // classinfo
+      24,   14, // methods
+       1,  180, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       7,       // signalCount
+
+ // signals: name, argc, parameters, tag, flags
+       1,    0,  134,    2, 0x06 /* Public */,
+       3,    0,  135,    2, 0x06 /* Public */,
+       4,    0,  136,    2, 0x06 /* Public */,
+       5,    0,  137,    2, 0x06 /* Public */,
+       6,    1,  138,    2, 0x06 /* Public */,
+       8,    1,  141,    2, 0x06 /* Public */,
+       9,    1,  144,    2, 0x06 /* Public */,
+
+ // slots: name, argc, parameters, tag, flags
+      10,    0,  147,    2, 0x08 /* Private */,
+      11,    1,  148,    2, 0x08 /* Private */,
+      12,    1,  151,    2, 0x08 /* Private */,
+
+ // methods: name, argc, parameters, tag, flags
+      15,    0,  154,    2, 0x02 /* Public */,
+      16,    0,  155,    2, 0x02 /* Public */,
+      18,    1,  156,    2, 0x02 /* Public */,
+      20,    0,  159,    2, 0x02 /* Public */,
+      21,    0,  160,    2, 0x02 /* Public */,
+      22,    0,  161,    2, 0x02 /* Public */,
+      23,    0,  162,    2, 0x02 /* Public */,
+      24,    0,  163,    2, 0x02 /* Public */,
+      25,    1,  164,    2, 0x02 /* Public */,
+      27,    1,  167,    2, 0x02 /* Public */,
+      29,    1,  170,    2, 0x02 /* Public */,
+      31,    1,  173,    2, 0x02 /* Public */,
+      32,    1,  176,    2, 0x02 /* Public */,
+      33,    0,  179,    2, 0x02 /* Public */,
+
+ // signals: parameters
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void, QMetaType::QVariantList,    7,
+    QMetaType::Void, QMetaType::Int,    7,
+    QMetaType::Void, QMetaType::Int,    7,
+
+ // slots: parameters
+    QMetaType::Void,
+    QMetaType::Void, QMetaType::Int,    7,
+    QMetaType::Void, 0x80000000 | 13,   14,
+
+ // methods: parameters
+    QMetaType::QVariantList,
+    0x80000000 | 17,
+    QMetaType::Void, QMetaType::QString,   19,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void, QMetaType::Int,   26,
+    QMetaType::Void, QMetaType::Bool,   28,
+    QMetaType::Void, QMetaType::Int,   30,
+    QMetaType::Void, QMetaType::Bool,   28,
+    QMetaType::Void, QMetaType::Int,   26,
+    QMetaType::Int,
+
+ // properties: name, type, flags
+      19, 0x80000000 | 17, 0x0009500b,
+
+       0        // eod
+};
+
+void QmlInterface::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        auto *_t = static_cast<QmlInterface *>(_o);
+        Q_UNUSED(_t)
+        switch (_id) {
+        case 0: _t->currentMusicIndexChanged(); break;
+        case 1: _t->musicIndexChanged(); break;
+        case 2: _t->shuffleChanged(); break;
+        case 3: _t->repeatChanged(); break;
+        case 4: _t->musicListSizeChanged((*reinterpret_cast< QVariantList(*)>(_a[1]))); break;
+        case 5: _t->playerPositionChanged((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 6: _t->musicDurationChanged((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 7: _t->onCurrentMusicIndexChanged(); break;
+        case 8: _t->onCurrentPlayerPositionChanged((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 9: _t->onCurrentMediaStatusChanged((*reinterpret_cast< QMediaPlayer::MediaStatus(*)>(_a[1]))); break;
+        case 10: { QVariantList _r = _t->getAllAudioFiles();
+            if (_a[0]) *reinterpret_cast< QVariantList*>(_a[0]) = std::move(_r); }  break;
+        case 11: { QDir _r = _t->getMusicPath();
+            if (_a[0]) *reinterpret_cast< QDir*>(_a[0]) = std::move(_r); }  break;
+        case 12: _t->setMusicPath((*reinterpret_cast< QString(*)>(_a[1]))); break;
+        case 13: _t->startPlaying(); break;
+        case 14: _t->pausePlaying(); break;
+        case 15: _t->stopPlaying(); break;
+        case 16: _t->playNext(); break;
+        case 17: _t->playPrevious(); break;
+        case 18: _t->removeFromPlaylist((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 19: _t->setShuffle((*reinterpret_cast< bool(*)>(_a[1]))); break;
+        case 20: _t->setPlaybackMode((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 21: _t->setPlayingState((*reinterpret_cast< bool(*)>(_a[1]))); break;
+        case 22: _t->setCurrentIndex((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 23: { int _r = _t->getCurrentMusicIndex();
+            if (_a[0]) *reinterpret_cast< int*>(_a[0]) = std::move(_r); }  break;
+        default: ;
+        }
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        switch (_id) {
+        default: *reinterpret_cast<int*>(_a[0]) = -1; break;
+        case 9:
+            switch (*reinterpret_cast<int*>(_a[1])) {
+            default: *reinterpret_cast<int*>(_a[0]) = -1; break;
+            case 0:
+                *reinterpret_cast<int*>(_a[0]) = qRegisterMetaType< QMediaPlayer::MediaStatus >(); break;
+            }
+            break;
+        }
+    } else if (_c == QMetaObject::IndexOfMethod) {
+        int *result = reinterpret_cast<int *>(_a[0]);
+        {
+            using _t = void (QmlInterface::*)();
+            if (*reinterpret_cast<_t *>(_a[1]) == static_cast<_t>(&QmlInterface::currentMusicIndexChanged)) {
+                *result = 0;
+                return;
+            }
+        }
+        {
+            using _t = void (QmlInterface::*)();
+            if (*reinterpret_cast<_t *>(_a[1]) == static_cast<_t>(&QmlInterface::musicIndexChanged)) {
+                *result = 1;
+                return;
+            }
+        }
+        {
+            using _t = void (QmlInterface::*)();
+            if (*reinterpret_cast<_t *>(_a[1]) == static_cast<_t>(&QmlInterface::shuffleChanged)) {
+                *result = 2;
+                return;
+            }
+        }
+        {
+            using _t = void (QmlInterface::*)();
+            if (*reinterpret_cast<_t *>(_a[1]) == static_cast<_t>(&QmlInterface::repeatChanged)) {
+                *result = 3;
+                return;
+            }
+        }
+        {
+            using _t = void (QmlInterface::*)(QVariantList );
+            if (*reinterpret_cast<_t *>(_a[1]) == static_cast<_t>(&QmlInterface::musicListSizeChanged)) {
+                *result = 4;
+                return;
+            }
+        }
+        {
+            using _t = void (QmlInterface::*)(int );
+            if (*reinterpret_cast<_t *>(_a[1]) == static_cast<_t>(&QmlInterface::playerPositionChanged)) {
+                *result = 5;
+                return;
+            }
+        }
+        {
+            using _t = void (QmlInterface::*)(int );
+            if (*reinterpret_cast<_t *>(_a[1]) == static_cast<_t>(&QmlInterface::musicDurationChanged)) {
+                *result = 6;
+                return;
+            }
+        }
+    }
+#ifndef QT_NO_PROPERTIES
+    else if (_c == QMetaObject::ReadProperty) {
+        auto *_t = static_cast<QmlInterface *>(_o);
+        Q_UNUSED(_t)
+        void *_v = _a[0];
+        switch (_id) {
+        case 0: *reinterpret_cast< QDir*>(_v) = _t->m_musicPath; break;
+        default: break;
+        }
+    } else if (_c == QMetaObject::WriteProperty) {
+        auto *_t = static_cast<QmlInterface *>(_o);
+        Q_UNUSED(_t)
+        void *_v = _a[0];
+        switch (_id) {
+        case 0:
+            if (_t->m_musicPath != *reinterpret_cast< QDir*>(_v)) {
+                _t->m_musicPath = *reinterpret_cast< QDir*>(_v);
+            }
+            break;
+        default: break;
+        }
+    } else if (_c == QMetaObject::ResetProperty) {
+    }
+#endif // QT_NO_PROPERTIES
+}
+
+QT_INIT_METAOBJECT const QMetaObject QmlInterface::staticMetaObject = { {
+    &QObject::staticMetaObject,
+    qt_meta_stringdata_QmlInterface.data,
+    qt_meta_data_QmlInterface,
+    qt_static_metacall,
+    nullptr,
+    nullptr
+} };
+
+
+const QMetaObject *QmlInterface::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *QmlInterface::qt_metacast(const char *_clname)
+{
+    if (!_clname) return nullptr;
+    if (!strcmp(_clname, qt_meta_stringdata_QmlInterface.stringdata0))
+        return static_cast<void*>(this);
+    return QObject::qt_metacast(_clname);
+}
+
+int QmlInterface::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QObject::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 24)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 24;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 24)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 24;
+    }
+#ifndef QT_NO_PROPERTIES
+    else if (_c == QMetaObject::ReadProperty || _c == QMetaObject::WriteProperty
+            || _c == QMetaObject::ResetProperty || _c == QMetaObject::RegisterPropertyMetaType) {
+        qt_static_metacall(this, _c, _id, _a);
+        _id -= 1;
+    } else if (_c == QMetaObject::QueryPropertyDesignable) {
+        _id -= 1;
+    } else if (_c == QMetaObject::QueryPropertyScriptable) {
+        _id -= 1;
+    } else if (_c == QMetaObject::QueryPropertyStored) {
+        _id -= 1;
+    } else if (_c == QMetaObject::QueryPropertyEditable) {
+        _id -= 1;
+    } else if (_c == QMetaObject::QueryPropertyUser) {
+        _id -= 1;
+    }
+#endif // QT_NO_PROPERTIES
+    return _id;
+}
+
+// SIGNAL 0
+void QmlInterface::currentMusicIndexChanged()
+{
+    QMetaObject::activate(this, &staticMetaObject, 0, nullptr);
+}
+
+// SIGNAL 1
+void QmlInterface::musicIndexChanged()
+{
+    QMetaObject::activate(this, &staticMetaObject, 1, nullptr);
+}
+
+// SIGNAL 2
+void QmlInterface::shuffleChanged()
+{
+    QMetaObject::activate(this, &staticMetaObject, 2, nullptr);
+}
+
+// SIGNAL 3
+void QmlInterface::repeatChanged()
+{
+    QMetaObject::activate(this, &staticMetaObject, 3, nullptr);
+}
+
+// SIGNAL 4
+void QmlInterface::musicListSizeChanged(QVariantList _t1)
+{
+    void *_a[] = { nullptr, const_cast<void*>(reinterpret_cast<const void*>(&_t1)) };
+    QMetaObject::activate(this, &staticMetaObject, 4, _a);
+}
+
+// SIGNAL 5
+void QmlInterface::playerPositionChanged(int _t1)
+{
+    void *_a[] = { nullptr, const_cast<void*>(reinterpret_cast<const void*>(&_t1)) };
+    QMetaObject::activate(this, &staticMetaObject, 5, _a);
+}
+
+// SIGNAL 6
+void QmlInterface::musicDurationChanged(int _t1)
+{
+    void *_a[] = { nullptr, const_cast<void*>(reinterpret_cast<const void*>(&_t1)) };
+    QMetaObject::activate(this, &staticMetaObject, 6, _a);
+}
+QT_WARNING_POP
+QT_END_MOC_NAMESPACE
